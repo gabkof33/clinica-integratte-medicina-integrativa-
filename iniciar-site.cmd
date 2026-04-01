d1247aebd031ac964f985dea3cce4d576d9aa5c8
gabkof33 <gabkof33@users.noreply.github.com>
2026-04-01 18:35:04 -0300

    Atualiza site

--- iniciar-site.cmd
+++ iniciar-site.cmd
@@ -1,4 +1,6 @@
 @echo off
 cd /d "%~dp0"
+set "NODE_EXE=%ProgramFiles%\nodejs\node.exe"
+if not exist "%NODE_EXE%" set "NODE_EXE=node"
 start "" cmd /c "timeout /t 2 >nul && start \"\" http://localhost:3000"
-node server.js
+"%NODE_EXE%" server.js
